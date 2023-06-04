1f7d55d7841988d2c5745dfd82224deeb7fd9626
DaniCP909 <d.corredorpadrino@gmail.com>
2023-06-04 09:01:04 +0200

    Update CommentController.java

--- OpenARS/src/main/java/dad/aplicacionweb/openars/controllers/CommentController.java
+++ OpenARS/src/main/java/dad/aplicacionweb/openars/controllers/CommentController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 //@Component
 @Controller
@@ -69,9 +70,55 @@ public class CommentController {
 
         System.out.println(usactual.getUsername() + " " + actual.getName() + " " + actual.getOwner().getEmail());
         //rabbitTemplate.convertAndSend(comment.getUser());
-        rabbitTemplate.convertAndSend("notifications", new CommentInfoDto(usactual.getUsername(), actual.getName(), actual.getOwner().getEmail()));
+        //rabbitTemplate.convertAndSend("notifications", new CommentInfoDto(usactual.getUsername(), actual.getName(), actual.getOwner().getEmail()));
 
         return "redirect:/all-resources/" + id;
+    }
+
+    @GetMapping("/{userid}/all-user-comments")
+    public String showUserComments(Model model, @PathVariable Long userid){
+
+        Optional<User> user = userServ.findById(userid);
+        if(user.isPresent()){
+            model.addAttribute("actualuser", user.get());
+        }
+
+        return "temps_Comment/all-uscomments";
+
+    }
+
+    @GetMapping("/{id}/edit-comment")
+    public String editComment(Model model, @PathVariable Long id){
+
+        Optional<Comment> comment = commentServ.findById(id);
+
+        if(comment.isPresent()){
+            model.addAttribute("comment", comment.get());
+        }
+        return "temps_Comment/edit-comment";
+    }
+
+    @PostMapping("/{id}/edit-comment")
+    public String editCommentPost(Model model, @PathVariable Long id, Comment comment){
+        Optional<Comment> optcomment = commentServ.findById(id);
+
+        comment.setUser(optcomment.orElseThrow().getUser());
+        comment.setResource(optcomment.orElseThrow().getResource());
+
+        commentServ.save(comment);
+
+
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/{id}/delete-comment")
+    public String deleteComment(Model model, @PathVariable Long id){
+        Optional<Comment> comment = commentServ.findById(id);
+        if(comment.isPresent()){
+            model.addAttribute("rscname", comment.get().getResource().getName());
+            commentServ.deleteById(id);
+        }
+        return "temps_Comment/removed-comment";
     }
 
 
