3b54ebe53c7cc6113c1f3429ec6c653f5a6eb1c4
danicp <d.corredorpadrino@gmail.com>
2023-06-05 18:31:45 +0200

    coment

--- OpenARS/start_db.sh
+++ OpenARS/start_db.sh
@@ -1,1 +1,1 @@
-docker run --rm -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=tesseract -p 3306:3306 -d mysql:8.0
+docker run --rm -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=tesseract -p 3307:3306 -d mysql:8.0
